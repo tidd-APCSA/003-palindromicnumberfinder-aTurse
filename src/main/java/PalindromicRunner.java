070e10359fbb5bcd2e3dea5261e5253c6815531c
Alex Turse <alexander@turse.pro>
2020-11-12 15:28:19 +0000

    added the difference calculation for distance

--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,7 +1,13 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
         // be sure you're testing each method as you work then produce your final output using the toString method
-        PalindromicNumberFinder test = new PalindromicNumberFinder(100);
+        int mileage = 72927;
+        PalindromicNumberFinder test = new PalindromicNumberFinder(mileage);
+        int milesToGo = test.searchForPalindromicNum(mileage) - mileage;
+        System.out.println(milesToGo);
+        
+
+        
 
     }
 }
