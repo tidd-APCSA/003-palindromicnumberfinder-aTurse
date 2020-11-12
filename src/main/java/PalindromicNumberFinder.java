fba49b586c800c40b8ed12af6d97fafd5ee2545f
Alex Turse <alexander@turse.pro>
2020-11-12 14:57:17 +0000

    Finished testing reverseNum & testPalindromicNum

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -11,28 +11,43 @@ public class PalindromicNumberFinder {
         return this.num;
     }
 
-    // this method should find the next palindromic number
+/*    // this method should find the next palindromic number
     public int searchForPalindromicNum(int num){
 
-    }
+      }
+*/
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
     public boolean testPalindromicNum(int num){
+      String reversed = reverseNum(num);
+      Integer number = num;
+      String stringNumber;
+      stringNumber = number.toString();
 
+      return stringNumber.equals(reversed);
 
     }
+
 
 
     // this is a helper method for testPalindromicNum. It should reverse the number and return it.
     public String reverseNum(int num){
-
-
+      
+      String reversedNumber = "";
+      Integer number = num;
+      String numberString = number.toString();
+        
+        for (int i = numberString.length() - 1; i >= 0; i--){
+          reversedNumber += numberString.substring(i, i + 1);
+        }
+      
+      return reversedNumber;
 
     }
 
 
-    @Override
-    public String toString(){
-        return "You'd have to drive " + searchForPalindromicNum(this.num) + " miles";
-    }
+    //@Override
+    //public String toString(){
+    //    return "You'd have to drive " + searchForPalindromicNum(this.num) + " miles";
+    //}
 }
--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,6 +1,8 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
         // be sure you're testing each method as you work then produce your final output using the toString method
+        PalindromicNumberFinder test1 = new PalindromicNumberFinder(100);
+        System.out.println(test1.testPalindromicNum(45234));
 
     }
 }
